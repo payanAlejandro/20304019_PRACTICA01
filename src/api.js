3ce7b81a2a18b862ed0f4e13c14850ba408b919e
Alejandro Payan <alejandro.payan@sigob.com.mx>
2024-02-22 20:17:17 -0700

    fix-test

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ app.get('/issues', (req, res) => {
 
 // Get all tasks
 app.get('/tasks', (req, res) => {
-  const tasks = [];//tasksRepository.getAll()
+  const tasks = tasksRepository.getAll()
   res.json(tasks);
 });
 
